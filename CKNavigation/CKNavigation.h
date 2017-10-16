2f070a1f0a71b8edbdd929b8674bbb6cb0aa836a
Charles Kenney <Charlesc.kenney@gmail.com>
2017-10-16 18:12:22 -0400

    Initial Commit

---
+++ CKNavigation/CKNavigation.h
@@ -0,0 +1,19 @@
+//
+//  CKNavigation.h
+//  CKNavigation
+//
+//  Created by Charles Kenney on 10/16/17.
+//  Copyright © 2017 Charles Kenney. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CKNavigation.
+FOUNDATION_EXPORT double CKNavigationVersionNumber;
+
+//! Project version string for CKNavigation.
+FOUNDATION_EXPORT const unsigned char CKNavigationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CKNavigation/PublicHeader.h>
+
+
